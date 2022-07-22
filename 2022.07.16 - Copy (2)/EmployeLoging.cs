c9e0c9a4a58d6942ab039c70728b636a1490ca04
tharakaMandushanka213 <tharakamandushanka12@gmail.com>
2022-07-22 11:49:11 +0530

    update

---
+++ 2022.07.16 - Copy (2)/EmployeLoging.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp7
+{
+    public partial class EmployeLoging  :Form
+
+        
+    {
+        
+
+        public EmployeLoging()
+        {
+            InitializeComponent();
+        }
+       
+
+        private void Logbtn_Click(object sender, EventArgs e, Form1 form1)
+        {
+            string index, password;
+
+           
+          
+             index = indexTexBox.Text;
+             password = PasswordtextBox.Text;
+
+                int val;
+            
+            if(index == "1234")
+            {
+                if(password == "12345")
+                {
+                    val = 1;
+                    form1.displaypanel(val);
+                     Close();
+                }
+                 else
+                 {
+                    MessageBox.Show("cant loging");
+                       
+                }
+            }
+            else
+             {
+                  MessageBox.Show("cant loging");
+             }
+
+        }
+    }
+}
---
+++ 2022.07.16 - Copy (2)/Register.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp7
+{
+    public partial class Register : Form
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+       
+
+            private void Register_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Submitbtn_Click(object sender, EventArgs e)
+        {
+            if(Email.Text.Length ==0)
+            {
+                string pattren = "^([0-9a-zA-Z]([-\\.\\w]*[0-9a-zA-Z]*@[0-9a-zA-Z][-\\w]*[0-9a-zA-Z]\\.)+[a-zA-Z]{2,9})$";
+                if (!Regex.IsMatch(Email.Text,pattren))
+                {
+                    MessageBox.Show("invalide email adderss","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }
+                else
+                {
+                    MessageBox.Show("valide email adderss", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+        }
+    }
+}
